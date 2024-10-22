a6c8c04226483a7527b25ca2fc4b5a8d95d76405
Alex Kit <alex.kit@atmajs.com>
2024-10-22 13:08:59 +0200

    fix (utest) server

--- src/clients/utils/TxFactory.ts
+++ src/clients/utils/TxFactory.ts
@@ -1,17 +1,5 @@
-// import {
-//     FeeMarketEIP1559Transaction,
-//     Transaction,
-//     TxData,
-//     FeeMarketEIP1559TxData,
-//     TxOptions
-// } from '@ethereumjs/tx';
+
 
 export namespace TxFactory {
-    // export function fromTxData (txData: TxData | FeeMarketEIP1559TxData, opts?: TxOptions) {
-    //     const Factory = txData.type && Number(txData.type) === 2
-    //         ? FeeMarketEIP1559Transaction
-    //         : Transaction
 
-    //     return Factory.fromTxData(txData as any, opts);
-    // }
 }
