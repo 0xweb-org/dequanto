5d3299d95fcd421b1688dd31b725bbd3395b5fec
Alex Kit <alex.kit@atmajs.com>
2025-06-19 21:19:56 +0200

    add (resolve secret) machine id from cli

--- src/utils/$secret.ts
+++ src/utils/$secret.ts
@@ -1,14 +1,14 @@
 import { $machine } from './$machine';
 
 export namespace $secret {
-    export async function getPin(parameters?: { pin?: string }) {
+    export async function getPin(parameters?: { pin?: string, machineKey?: string }) {
 
         let pin = parameters?.pin ?? getPinFromCli() ?? getPinFromEnv();
         if (pin == null || pin.length === 0) {
             return null;
         }
-        let id = await $machine.id();
-        return `${id}:${pin}`;
+        let machineKey = parameters?.machineKey ?? getMachineKeyFromCli() ?? getMachineKeyFromEnv() ?? await $machine.id();
+        return `${machineKey}:${pin}`;
     }
 
     function getPinFromCli () {
@@ -23,5 +23,18 @@ export namespace $secret {
     }
     function getPinFromEnv () {
         return process.env.PIN;
+    }
+    function getMachineKeyFromCli () {
+        let args = process.argv;
+        for (let i = 0; i < args.length - 1; i++) {
+            let key = args[i].replace(/^\-+/, '');
+            if (key === 'machineKey' || key ==='machine-key') {
+                return args[i + 1];
+            }
+        }
+        return null;
+    }
+    function getMachineKeyFromEnv () {
+        return process.env.MACHINE_KEY;
     }
 }
