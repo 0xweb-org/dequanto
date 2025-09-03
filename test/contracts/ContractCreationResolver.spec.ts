366b88c1d58ebda54b1e28f573a5b35828078f81
unknown <alex.kit@atmajs.com>
2025-09-03 23:10:58 +0200

    'bump'

--- test/contracts/ContractCreationResolver.spec.ts
+++ test/contracts/ContractCreationResolver.spec.ts
@@ -14,7 +14,7 @@ UTest({
         let info = await resolver.getInfo(address);
         eq_(info.tx, '0xc4c1840db940f5075c5404266efe50d7c65bc079f318ab8e5a10b8a432b49d30');
         eq_(info.block, 10042267);
-        eq_(info.timestamp, 1589164213000);
+        eq_(info.timestamp, 1589164213);
     },
     async 'should throw if not a contract' () {
         let address = `0xa478c2975ab1ea89e8196811f51a7b7ade33eb00` as const;
