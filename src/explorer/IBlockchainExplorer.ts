63239fb0c679af3031dfc79917bc6bd204687d9b
Alex <alex.kit@atmajs.com>
2026-09-25 12:12:55 +0200

    fix (type) optional field

--- src/explorer/IBlockchainExplorer.ts
+++ src/explorer/IBlockchainExplorer.ts
@@ -7,8 +7,8 @@ import { TExplorerDefinition } from '@dequanto/models/TExplorer';
 
 
 export interface IBlockchainExplorer extends IAbiProvider, IVerifier {
-    config: {
-        api: { url?: string; key?: string }
+    config?: {
+        api?: { url?: string; key?: string }
     }
     inMemoryDb: IContractDetails[]
     getContractMeta(q: string): Promise<IContractDetails>;
